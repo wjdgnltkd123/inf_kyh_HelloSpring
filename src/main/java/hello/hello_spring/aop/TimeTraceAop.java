9ed1ba8d0bb7a355fa368341a8161246a3948000
user <wjdgnltkd123@gmail.com>
2025-01-03 14:02:01 +0900

    Changes

---
+++ src/main/java/hello/hello_spring/aop/TimeTraceAop.java
@@ -0,0 +1,22 @@
+package hello.hello_spring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceAop {
+    @Around("execution(* hello.hello_spring..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long endTime = System.currentTimeMillis();
+            long elapsedTime = endTime - startTime;
+            System.out.println("End " + joinPoint.toString() + " Elapsed time: " + elapsedTime + " ms");
+        }
+    }
+}
